227f63f11c676b90096065f5f1ed0cd3c751e7e7
neurlang <77860779+neurlang@users.noreply.github.com>
2026-06-17 16:16:59 +0200

    Refactor new api

--- go/noareg/transformer_infer.go
+++ go/noareg/transformer_infer.go
@@ -39,43 +39,12 @@ func TransformerInfer(transformer *NoaregTransformer, input []uint32) (output []
 	return createTokensFrom2DFloat32(output_floats), nil
 }
 
-func TransformerInferFull(transformer *NoaregTransformer, detokenizer map[uint32]map[[2]uint32]string, try_word string) (out string, garbage [2]string, err error) {
-
-	const limit = 16
-
-	pseudo_data, garbage := Tokenize(detokenizer, try_word, limit)
-	var move = limit
-	for i := 0; i+limit < len(pseudo_data); i += move {
-
-		//fmt.Println(pseudo_data[i:i+limit])
-		output_data, err := TransformerInfer(transformer, pseudo_data[i:i+limit])
-		if err != nil {
-			return "", [2]string{}, err
-		}
-
-		//fmt.Println(output_data)
-		for j := range output_data {
-			if pseudo_data[i : i+limit][j] == 0 {
-				move = limit
-				break
-			}
-			var part = GetDetokenizerChoice(detokenizer, pseudo_data[i : i+limit][j], output_data[j])
-			if strings.HasPrefix(part, "_") {
-				if j == 0 {
-					move = j + 1
-				} else {
-					move = j
-				}
-				break
-			}
-			out += part
-			if strings.HasSuffix(part, "_") {
-				move = j + 1
-				break
-			}
-		}
+func TransformerInferFull(transformer *NoaregTransformer, detokenizer map[uint32]map[[2]uint32]string, try_word string) (out2 string, garbage [2]string, err error) {
+	out, garbage, err := TransformerInferFull2(transformer, detokenizer, try_word)
+	for _, v := range out {
+		out2 += v[1]
 	}
-	return out, garbage, nil
+	return out2, garbage, err
 }
 
 
